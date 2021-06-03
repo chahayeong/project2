be4184c6ee016115bde7f2cc51e3d81cd388baec
chahayeong <c6prince@naver.com>
2021-06-03 17:22:35 +0900

    project1

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <hav>
+    
+    </hav>
+
+    <h1>헤드라인1</h1>
+    <h2>헤드라인2</h2>
+    <h3>헤드라인3</h3>
+    <h4>헤드라인</h4>
+    <h5>헤드라인</h5>
+    <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 것이겠죠?</p>
+    <a href="https://www.google.com">Go to google</a>
+    <hr/>
+    <img src="./images/stay_funky.jpg" width="600px">
+    </img>
+    </>
+  );
+}
+
+export default App;
